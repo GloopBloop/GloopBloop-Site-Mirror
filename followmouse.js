a0501f70d0082f04650135f48cf7afcd360153f3
GloopBloop <gloopbloop113@gmail.com>
2023-05-01 20:11:30 -0400

    Added font, mouse effect, and header

---
+++ followmouse.js
@@ -0,0 +1,41 @@
+// http://ahrengot.com/tutorials/greensock-javascript-animation
+
+/*
+The MIT License (MIT)
+
+Copyright (c) 2023 GRAY GHOST (https://codepen.io/grayghostvisuals/pen/AJQgeB)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+// ^^ yoinked this code from online and it came with that license
+// thank you gray ghost i now get to have le funny circle
+
+var $circle = $('.circle');
+
+function moveCircle(e) {
+  TweenLite.to($circle, 0.3, {
+    css: {
+      left: e.pageX,
+      top: e.pageY
+    }
+  });
+}
+
+$(window).on('mousemove', moveCircle);
